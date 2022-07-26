7e9c4675ee9008ffe9190a0d553e5e32456329b1
Lucidreline <casta.ma502@gmail.com>
2022-07-26 15:31:02 -0700

    Added .gitignore.

---
+++ temperatureServer.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify 
+import Adafruit_DHT
+from time import sleep
+
+sensor = Adafruit_DHT.DHT11
+pin = 4
+
+app = Flask(__name__) # creates an instance of Flask; the argument is the applications module/package
+
+@app.route("/") # tells flask what url should trigger our function
+def hello_world():
+  return "<p>You found me! Qpz</p>" # this html will show on the user's browser
+
+@app.route("/temperature")
+def getTemperatureAndHumidity():
+    nullResponse = True
+    while nullResponse is True:
+        humidity, temperature = Adafruit_DHT.read(sensor, pin)
+        if temperature is None:
+            sleep(1)
+        else:
+            return jsonify(
+                temperature=temperature,
+                humidity=humidity
+    )
+
